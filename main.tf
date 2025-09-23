12043c9920d35d8a1076c2e44ac40b5c38223fad
Justin Odel <justinodel@pgatourhq.com>
2025-09-23 15:21:35 -0400

    setup backend state

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "tww-terraform-state-files-root"
+    key    = "Account-Infrastructure-Orchestration/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
