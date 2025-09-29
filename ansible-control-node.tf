ba689b6dba04cb50eacaa5481c063dc3853d144b
Matthew Brannon <mattherpbrannon@gmail.com>
2025-09-29 09:39:23 -0400

    scaling down to t2

--- ansible-control-node.tf
+++ ansible-control-node.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "ansible_control_node" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 
   user_data     = filebase64("${path.root}/scripts/ansible-control-userdata.sh")
 
