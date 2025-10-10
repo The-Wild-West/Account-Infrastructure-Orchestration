7d77968304eb67e2072fc8557faf50bd7e47ca96
Justin Odel <odel.justin@gmail.com>
2025-10-10 15:13:30 -0400

    copying assume role script

--- scripts/ansible-control-assumerole.sh
+++ scripts/ansible-control-assumerole.sh
@@ -1,4 +1,4 @@
-role=$(aws sts assume-role --role-arn "arn:aws:iam::856364870650:role/ansible_cross_account_role" --role-session-name "Ansible-Control-Session")
+role=$(aws sts assume-role --role-arn $1 --role-session-name "Ansible-Control-Session")
 export AWS_ACCESS_KEY_ID=$(echo $role | jq -r '.Credentials.AccessKeyId')
 export AWS_SECRET_ACCESS_KEY=$(echo $role | jq -r '.Credentials.SecretAccessKey')
 export AWS_SESSION_TOKEN=$(echo $role | jq -r '.Credentials.SessionToken')
