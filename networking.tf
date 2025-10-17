8bf5ee94fd10960d2398acf0e54844cbad016c6a
Justin Odel <odel.justin@gmail.com>
2025-10-17 10:33:53 -0400

    updated seg to allow all traffic egress

--- networking.tf
+++ networking.tf
@@ -56,28 +56,14 @@ resource "aws_route_table_association" "public" {
 }
 
 resource "aws_security_group" "sg" {
-  name        = "root_sg"
+  name        = "Allow_All_Egress_SG"
   vpc_id      = aws_vpc.main.id
-  description = "main sg in the root account"
+  description = "Allow all ports and ips egress rule"
 
   egress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
